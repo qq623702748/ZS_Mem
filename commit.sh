b022090954fed05427fa878f8528db8c9aa8ce8e
zhuchaodi <623702748@qq.com>
2017-03-01 11:30:30 +0800

    修正shell脚本

--- commit.sh
+++ commit.sh
@@ -3,11 +3,8 @@ modify=`git status | grep "modified:\|新增:" | awk '{print $2}'`
 
 if [ -z modify ]
 then
-    echo '新增的文件是:'$2
-    git add $2
-    git commit -m $2
     git pull
-    git push
+    echo "nothing change!"
 else
     echo '修改的文件是:'$modify
     git add $modify
